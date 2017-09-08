a70f3ec00f1869069e8c28f89a17e3bd7b6bc2be
Darragh Jones <darragh@lexa.tech>
2017-09-08 14:11:43 +0100

    updated server url

--- src/pages/constants/server.js
+++ src/pages/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-  ? 'http://myapidomain.com/payment'
+  ? 'https://apps.barnkids.co.uk/payment'
   : 'http://localhost:3000/payment';
 
 export default PAYMENT_SERVER_URL;
