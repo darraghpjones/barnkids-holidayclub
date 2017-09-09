4236ec26ae8d703d9625eb16bbf94f32a85eea93
Darragh Jones <darragh@lexa.tech>
2017-09-10 00:12:54 +0100

    adding payment config

--- ticket-backend/constants/stripe.js
+++ ticket-backend/constants/stripe.js
@@ -1,7 +1,7 @@
 const configureStripe = require('stripe');
 
 const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-    ? 'sk_live_MY_SECRET_KEY'
+    ? 'sk_live_P4qdlqHVllp6vfYsf5w0Uh8E'
     : 'sk_test_gZQDbT2ymZESi0FT5N45eQ5J';
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
