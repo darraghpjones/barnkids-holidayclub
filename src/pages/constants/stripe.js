3d6c850622d3c90adf268ee3e63c9950dd595411
darraghpjones <darragh@u-cloud.com>
2018-02-28 12:28:39 +0000

    Delete stripe.js

--- src/pages/constants/stripe.js
+++
@@ -1,5 +0,0 @@
-const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-  ? 'pk_live_9zQrlxm90E7Ecb31ad6ZexGC'
-  : 'pk_test_TDKKjTNKqYZOyFQVmD7MU46D';
-
-export default STRIPE_PUBLISHABLE;
